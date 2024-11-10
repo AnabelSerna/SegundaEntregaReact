b218d3ae7ff9163fb5d7f1212a90d3f28a6d85fa
anabelserna <anabelserna49@gmail.com>
2024-11-10 13:17:20 -0300

    entregaFinalReact

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import ItemCount from "../ItemCount/ItemCount"
-import "./itemdetail.css"
+import '../ItemDetailContainer/itemdetail.css'
 
 const ItemDetail = ({ product, addProduct, hideItemCount }) => {
   return (
